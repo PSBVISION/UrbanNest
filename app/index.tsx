32e68bd04b242e676fead547b412b379b2cc1702
PSBVISION <punitsinghbisht1529@gmail.com>
2026-06-07 23:22:18 +0530

    feat: implement authentication layout and sign-in/sign-up screens

--- app/index.tsx
+++ app/index.tsx
@@ -1,17 +1,25 @@
+import { useAuth } from "@clerk/expo";
+import { Redirect } from "expo-router";
 import { Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
-  return (
-    <SafeAreaView
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundColor: "#222",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </SafeAreaView>
-  );
+  const{isSignedIn, isLoaded} = useAuth();
+  if (!isLoaded) {
+    return (
+      <SafeAreaView
+        style={{
+          flex: 1,
+          justifyContent: "center",
+          alignItems: "center",
+          backgroundColor: "#222",
+        }}
+      >
+        <Text>Loading...</Text>
+      </SafeAreaView>
+    );
+  }
+
+  if (isSignedIn) return <Redirect href="/(root)/(tabs)" />;
+  return <Redirect href="/sign-in" />;
 }
